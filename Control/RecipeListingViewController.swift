c91b547e153364dafd444d5b3b83a7baed0c988c
Pamela <pamelalim23@gmail.com>
2023-05-09 15:14:38 +1000

    RecipeViewListingViewController created

---
+++ Control/RecipeListingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListingViewController.swift
+//  myRecipes
+//
+//  Created by Pamela Lim on 9/5/23.
+//
+
+import UIKit
+import Foundation
+
+
+class RecipeListingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
