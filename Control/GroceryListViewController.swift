2cbf7e04a6eee9cf5447afe660606670dcb3cc94
thesyaevania <104770684+thesyaevania@users.noreply.github.com>
2023-05-09 14:37:11 +1000

    GroceryList segue

---
+++ Control/GroceryListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GroceryListViewController.swift
+//  myRecipes
+//
+//  Created by Thesya Evania on 9/5/2023.
+//
+import UIKit
+class GroceryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Control/RecipeListingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListingViewController.swift
+//  myRecipes
+//
+//  Created by Thesya Evania on 9/5/2023.
+//
+
+import UIKit
+
+class RecipeListingViewController: UIViewController {
+    @IBAction func cartButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
