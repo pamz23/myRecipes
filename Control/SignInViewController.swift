13ed615cc6af3ad11a5a093fb841bc8271e3c5db
Stine Juhl <stine.juhl@student.uts.edu.au>
2023-05-09 14:56:30 +1000

    fixed the viewControllers

--- Control/SignInViewController.swift
+++ Control/SignInViewController.swift
@@ -10,8 +10,15 @@ import Foundation
 
 class SignInViewController: UIViewController {
     
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var signIn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
     }
 }
