c395fac82971b1a856d25d1f33925ab003c814a9
Stine Juhl <stinejuhl00@gmail.com>
2023-05-09 12:12:25 +1000

    Created three views for welcome page, logIn and signUp

---
+++ Control/LogInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LogInViewController.swift
+//  myRecipes
+//
+//  Created by Stine Juhl on 9/5/2023.
+//
+
+import UIKit
+import Foundation
+
+class LogInViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var signIn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
