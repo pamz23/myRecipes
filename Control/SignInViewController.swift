a43dc0bed72811e39c19898253e7c67dae0d7460
Stine Juhl <stine.juhl@students.uts.edu.au>
2023-05-15 11:10:28 +1000

    Added new code to SignInViewController and SignUpViewController

--- Control/SignInViewController.swift
+++ Control/SignInViewController.swift
@@ -16,9 +16,45 @@ class SignInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        // Add target to signInButton to trigger signInButtonTapped(_:)
+        signIn.addTarget(self, action: #selector(signInButtonTapped(_:)), for: .touchUpInside)
     }
     
-    @IBAction func signIn(_ sender: Any) {
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+        // Perform sign-in logic here
+        guard let email = email.text, !email.isEmpty else {
+            showAlert(withTitle: "Error", message: "Please enter your username.")
+            return
+        }
+    
+        guard let password = password.text, !password.isEmpty else {
+            showAlert(withTitle: "Error", message: "Please enter your password.")
+            return
+        }
+    
+        // Validate the username and password
+        if validateCredentials(email: email, password: password) {
+            // Successful sign-in
+            showAlert(withTitle: "Success", message: "Sign-in successful!")
+            // Navigate to the next screen or perform any necessary actions
+        } else {
+            // Failed sign-in
+            showAlert(withTitle: "Error", message: "Invalid username or password.")
+        }
+    }
+
+    func validateCredentials(email: String, password: String) -> Bool {
+        let validEmail = "myemail"
+        let validPassword = "mypassword"
+        
+        return email == validEmail && password == validPassword
+    }
+
+    func showAlert(withTitle title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
--- Control/SignUpViewController.swift
+++ Control/SignUpViewController.swift
@@ -19,7 +19,36 @@ class SignUpViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func signUp(_ sender: Any) {
+    
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
+        // Check if all fields are filled
+        guard  let name = yourName.text,
+               let email = yourEmail.text,
+               let password = youPassword.text,
+               !name.isEmpty,
+               !email.isEmpty,
+               !password.isEmpty else {
+            print("Error, please fill in all the fields")
+            return
+        }
+        
+        // Perform signup process
+        performSignup(with: name, email: email, password: password)
     }
+    
+    func performSignup(with name: String, email: String, password: String) {
+        // Your signup logic here
+        // This is where you would typically make an API call to register the user
+        
+        
+        // Show success message
+        print("Sign up was successful")
+    }
+        
+        func showAlert(with title: String, message: String) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
 }
-
