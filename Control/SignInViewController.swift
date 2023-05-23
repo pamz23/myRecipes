aea57bcdb1cc89d264699d6cff7233d8d28375a3
Stine Juhl <stine.juhl@student.uts.edu.au>
2023-05-23 12:08:41 +1000

    SignIn logic

--- Control/SignInViewController.swift
+++ Control/SignInViewController.swift
@@ -17,52 +17,77 @@ class SignInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Add target to signInButton to trigger signInButtonTapped(_:)
+        
+        // Disable the sign-in button initially
+        signIn.isEnabled = false
+        
+        // Add target actions for text fields to handle editing changes
+        username.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        email.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        password.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         signIn.addTarget(self, action: #selector(signInButtonTapped(_:)), for: .touchUpInside)
     }
     
+    // Handle editing changes in text fields
+        @objc func textFieldDidChange(_ textField: UITextField) {
+            // Check if all fields have values
+            let username = username.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+            let email = email.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+            let password = password.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+
+            // Enable the sign-in button only if all fields have non-empty values
+            signIn.isEnabled = !username.isEmpty && !email.isEmpty && !password.isEmpty
+        }
+    
+    // Action for sign-in button
     @IBAction func signInButtonTapped(_ sender: UIButton) {
-        // Perform sign-in logic here
-        
+ 
+        //checks if username textfield is empty
         guard let username = username.text, !username.isEmpty else {
             showAlert(withTitle: "Error", message: "Please enter yuor username.")
             return
         }
-        
+        //checks if email textfield is empty
         guard let email = email.text, !email.isEmpty else {
             showAlert(withTitle: "Error", message: "Please enter your username.")
             return
         }
-    
+        //checks if password textfield is empty
         guard let password = password.text, !password.isEmpty else {
             showAlert(withTitle: "Error", message: "Please enter your password.")
             return
         }
-    
-        // Validate the username and password
-        if validateCredentials(username: username, email: email, password: password) {
+
+        // Validate the username, email, and password
+        if validateCredentials(username: username, email: email, password: password, <#UITextField#>) {
             // Successful sign-in
             showAlert(withTitle: "Success", message: "Sign-in successful!")
             // Navigate to the next screen or perform any necessary actions
         } else {
             // Failed sign-in
-            showAlert(withTitle: "Error", message: "Invalid username or password.")
+            showAlert(withTitle: "Error", message: "Invalid username, email or password.")
         }
     }
 
-    func validateCredentials(username: String, email: String, password: String) -> Bool {
+    //validates the credentials for all texifields
+    func validateCredentials(username: String, email: String, password: String, _ textField: UITextField) -> Bool {
         let validUsername = "myusername"
         let validEmail = "myemail"
         let validPassword = "mypassword"
         
-        return username == validUsername && email == validEmail && password == validPassword
-        
-     /*   // Make a request to authentication service or backend API for validation
-        Dictionary().authenticate(username: username, password: password) { result in
-            // Hide the loading indicator or enable UI    }*/
+        //deletes backwards if the count of charactes in textfield is more than 10
+        if let currentText = textField.text {
+            if currentText.count > 10 {
+                textField.deleteBackward()
+            } else {
+                textField.resignFirstResponder()
+            }
+            
+            return username == validUsername && email == validEmail && password == validPassword
+        }
     }
 
+    //function to display of alert with title and message to the user
     func showAlert(withTitle title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
