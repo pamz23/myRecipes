3107e95caac7e825d788b74a891fa747623987fe
Stine Juhl <stine.juhl@student.uts.edu.au>
2023-05-23 12:59:38 +1000

    Some SignUp logic added

--- Control/SignUpViewController.swift
+++ Control/SignUpViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Foundation
 
+
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var yourName: UITextField!
@@ -15,11 +16,50 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var youPassword: UITextField!
     @IBOutlet weak var signUp: UIButton!
     
+    var users: [[String: String]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Disable the sign-up button initially
+            signUp.isEnabled = false
+
+        // Add target actions for text fields to handle editing changes
+        yourName.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        yourEmail.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        youPassword.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
     }
     
+    // Handle editing changes in text fields
+        @objc func textFieldDidChange(_ textField: UITextField) {
+            // Check if all fields have values
+            let name = yourName.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+            let email = yourEmail.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+            let password = youPassword.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+
+            // Enable the sign-up button only if all fields have non-empty values
+            signUp.isEnabled = !name.isEmpty && !email.isEmpty && !password.isEmpty
+        }
+    
+    // Function to check if a user with a specific username, email, or password exists in the dictionary
+    func isUserExists(name: String?, email: String?, password: String?) -> Bool {
+        for user in users {
+            if let nameToCheck = name, let userName = user["name"], nameToCheck == userName {
+                return true
+            }
+            
+            if let emailToCheck = email, let userEmail = user["email"], emailToCheck == userEmail {
+                return true
+            }
+            
+            if let passwordToCheck = password, let userPassword = user["password"], passwordToCheck == userPassword {
+                return true
+            }
+        }
+        return false
+    }
+
     @IBAction func signupButtonTapped(_ sender: UIButton) {
         // Check if all fields are filled
         guard  let username = yourName.text,
