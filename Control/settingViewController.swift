5f45d51f853764b838ce876dcf29288f43c3c2e2
Sajiz <90175427+Sajiz@users.noreply.github.com>
2023-05-25 14:57:36 +1000

    round profile photo + constraints set (hardcoded)

--- Control/settingViewController.swift
+++ Control/settingViewController.swift
@@ -25,6 +25,18 @@ class settingViewController: UIViewController, UIImagePickerControllerDelegate, 
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateNameLabel()
+        
+        // Make the profile photo view circular
+           profilePhotoImgView.layer.cornerRadius = profilePhotoImgView.frame.size.width / 2
+           profilePhotoImgView.clipsToBounds = true
+           
+           // Set constraints for the image view
+           profilePhotoImgView.contentMode = .scaleAspectFill
+           profilePhotoImgView.translatesAutoresizingMaskIntoConstraints = false
+           profilePhotoImgView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+           profilePhotoImgView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+           profilePhotoImgView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 240).isActive = true
+        profilePhotoImgView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -1).isActive = true
     }
     
     private func updateNameLabel() {
