5e93e51d0d48bc1e7b1194ec27fb0dd7588b4bdf
Sajiz <90175427+Sajiz@users.noreply.github.com>
2023-05-29 17:36:20 +1000

    neater code + comments on settingViewController

--- Control/settingViewController.swift
+++ Control/settingViewController.swift
@@ -26,51 +26,54 @@ class settingViewController: UIViewController, UIImagePickerControllerDelegate, 
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateNameLabel()
+        formatProfilePhotoImgView()
+    }
+    
+    private func formatProfilePhotoImgView()
+    {
+        //load in placeholder image and display it in the Image View until the user uploads their profile photo
         let placeholderImage = UIImage(named: "placeholder_image")
         profilePhotoImgView.image = placeholderImage
  
         // Make the profile photo view circular
-           profilePhotoImgView.layer.cornerRadius = profilePhotoImgView.frame.size.width / 2
-           profilePhotoImgView.clipsToBounds = true
+        profilePhotoImgView.layer.cornerRadius = profilePhotoImgView.frame.size.width / 2
+        profilePhotoImgView.clipsToBounds = true
            
            // Set constraints for the image view
-           profilePhotoImgView.contentMode = .scaleAspectFill
-           profilePhotoImgView.translatesAutoresizingMaskIntoConstraints = false
-           profilePhotoImgView.widthAnchor.constraint(equalToConstant: 120).isActive = true
-           profilePhotoImgView.heightAnchor.constraint(equalToConstant: 120).isActive = true
-           profilePhotoImgView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 240).isActive = true
+        profilePhotoImgView.contentMode = .scaleAspectFill
+        profilePhotoImgView.translatesAutoresizingMaskIntoConstraints = false
+        profilePhotoImgView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        profilePhotoImgView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        profilePhotoImgView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 240).isActive = true
         profilePhotoImgView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -1).isActive = true
-        
     }
-    
-    
     
     private func updateNameLabel() {
         if let username = username {
-                    nameLabel.text = username
+                    nameLabel.text = username //update nameLabel with username that was entered during SignUp/SignIn process
                 }
         }
     
-    @IBAction func changeNameBtnTapped(_ sender: UIButton) {
+    @IBAction func changeNameBtnTapped(_ sender: UIButton) { //If the changeName button is tapped, display an alert text field where the user can enter and save thier new name.
         let alertController = UIAlertController(title: "Change Name", message: "Enter your new name", preferredStyle: .alert)
         alertController.addTextField { textField in
-            textField.placeholder = "New Name"
+            textField.placeholder = "New Name" //show Alert
         }
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] _ in
             guard let textField = alertController.textFields?.first,
-                  let newName = textField.text,
+                  let newName = textField.text, //Save whatever is entered in textfield as newName.
                   !newName.isEmpty else {
                 return
             }
             
             self?.username = newName
-            self?.updateNameLabel()
+            self?.updateNameLabel() //update nameLabel to show the new name.
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
                 
         alertController.addAction(saveAction)
-        alertController.addAction(cancelAction)
+        alertController.addAction(cancelAction) //if the cancel button is pressed instead of save, cancel the action entirely.
                 
         present(alertController, animated: true, completion: nil)
     }
@@ -79,12 +82,12 @@ class settingViewController: UIViewController, UIImagePickerControllerDelegate, 
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
+        present(imagePicker, animated: true, completion: nil) //if the changePhoto button is pressed, run the imagePicker function, which allows the user to select a photo from the iPhone's gallery.
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[.originalImage] as? UIImage {
-            profilePhotoImgView.image = selectedImage
+            profilePhotoImgView.image = selectedImage //replace imageView with selected image from gallery.
             changePhotoBtn.setTitle("Change your Photo", for: .normal)
         }
         
@@ -92,10 +95,10 @@ class settingViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
+        picker.dismiss(animated: true, completion: nil) //if the cancel button is pressed instead of save, cancel the action entirely.
     }
     
-    //dark mode toggle
+    //Dark mode toggle - depending on the value of the UISwitch, either toggle dark/light theme application wide. 
     @IBAction func darkModeSwitchToggled(_ sender: UISwitch) {
         if sender.isOn {
                     if let window = UIApplication.shared.windows.first {
